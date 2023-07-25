201dadd275d7f8c2067e8615ac34acc5a056fbce
vigyavigy <81960640+vigyavigy@users.noreply.github.com>
2023-07-25 10:54:04 +0530

    Add files via upload

---
+++ levelordertraversal.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+//function to print binary tree
+node* buildtree(node* root){
+    cout<<"enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+
+    if(data==-1) return NULL;
+    cout<<"enter data for inserting in left: "<<endl;
+    root->left=buildtree(root->left);
+    cout<<"enter data for inserting in right: "<<endl;
+    root->right=buildtree(root->right);
+    return root;
+}
+void levelordertraversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            //previous level traversal is completed
+            cout<<endl;
+            if(!q.empty()) q.push(NULL);
+        }
+        else{
+                    cout<<temp->data<<" ";
+            if(temp->left)q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+}
+void inorder(node* root){
+    if(root ==NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+int main(){
+    node* root=NULL;
+    //creating a tree
+    root=buildtree(root);
+    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    //level order
+    cout<<"level order traversal is: "<<endl;
+    levelordertraversal(root);
+}
+//to print level order traversal in levels we need a separator and that separator is NULL
